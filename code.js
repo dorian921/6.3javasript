a1af86a44c764d1d1ae21a2e179ed93e0e0d267e
dorian921 <144111488+dorian921@users.noreply.github.com>
2023-10-11 09:19:45 +0200

    Add files via upload

---
+++ code.js
@@ -0,0 +1,9 @@
+function veranderkleur(kleur) {
+document.getElementById('kop1').style.color = kleur;
+
+}
+
+function verandertekst(tekst) {
+    document.getElementById('kop1').innerHTML = "inhoud aangepast"
+}
+
